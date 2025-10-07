db3398fa6b173bb0d64fe04627942a89a54038ec
TiviRomer <stevenromeros2005@gmail.com>
2025-10-06 21:31:18 -0600

    Revert "Piramides de xd"

--- Backend/hello.py
+++ Backend/hello.py
@@ -1,12 +1,3 @@
 for _ in range(10):
     print("hola mundo")
     print("xd")
-    print("xdxd")
-    print("xdxdxd")
-    print("xdxdxdxd")
-    print("xdxdxdxdxd")
-    print("xdxdxdxdxdxd")
-    print("xdxdxdxdxdxdxd")
-    print("xdxdxdxdxdxdxdxd")
-    print("xdxdxdxdxdxdxdxdxd")
-    print("xdxdxdxdxdxdxdxdxdxd")
